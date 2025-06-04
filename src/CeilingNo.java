c3904f773e1c08c1f521453ddd0523f741342927
BasavarajSK <basukori2278@gmail.com>
2025-06-04 11:39:16 +0530

    first commit: Ceiling of an number problem , given sorted array go with binary search.

---
+++ src/CeilingNo.java
@@ -0,0 +1,33 @@
+public class CeilingNo {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,6,9,11,14,17,19};
+        int target = 10;
+        // Ceiling of the number the smallest element which is greater than or equal to given target...
+       int ceilingNo =  findCeilingNo(arr, target);
+        System.out.println("Ceiling of the number "+ target + "is: "+ ceilingNo);
+    }
+
+    static int findCeilingNo(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        if(target > arr[end]){
+            return -1;
+        }
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if(target > arr[mid]){
+                start = mid + 1;
+            } else if(target < arr[mid]){
+                end = mid - 1;
+            } else {
+                return arr[mid];
+            }
+        }
+        return arr[start];
+    }
+}
